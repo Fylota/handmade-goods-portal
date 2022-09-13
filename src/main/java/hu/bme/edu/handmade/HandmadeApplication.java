f709d53f0cc246f5740f55a61f43dadce2cbf41c
Fylota <fulop.agota01@gmail.com>
2022-09-13 14:13:03 +0200

    Initialized Spring project

---
+++ src/main/java/hu/bme/edu/handmade/HandmadeApplication.java
@@ -0,0 +1,13 @@
+package hu.bme.edu.handmade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandmadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandmadeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/bme/edu/handmade/HandmadeApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.bme.edu.handmade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandmadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
