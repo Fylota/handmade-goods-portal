95f04d59ea19a961d976066d99082e2c4c271010
Ágota Fülöp <fulop.agota01@gmail.com>
2022-10-01 20:59:35 +0200

    Added basic tests

---
+++ src/test/java/hu/bme/edu/handmade/SmokeTest.java
@@ -0,0 +1,18 @@
+package hu.bme.edu.handmade;
+
+import hu.bme.edu.handmade.controllers.HomeController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class SmokeTest {
+    @Autowired
+    private HomeController controller;
+
+    @Test
+    public void contextLoads() throws Exception {
+        assertThat(controller).isNotNull();
+    }
+}
