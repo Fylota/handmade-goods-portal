cf097e3dc9e26f4d4a76d2c0ff50afa1051b3c0f
Ágota Fülöp <fulop.agota01@gmail.com>
2022-12-11 18:05:11 +0100

    Refactoring HomeController

--- src/main/java/hu/bme/edu/handmade/controllers/HomeController.java
+++ src/main/java/hu/bme/edu/handmade/controllers/HomeController.java
@@ -1,17 +1,21 @@
 package hu.bme.edu.handmade.controllers;
 
+import hu.bme.edu.handmade.model_assemblers.UserModelAssembler;
+import hu.bme.edu.handmade.models.User;
 import hu.bme.edu.handmade.security.JwtRequest;
 import hu.bme.edu.handmade.security.JwtResponse;
 import hu.bme.edu.handmade.security.JwtTokenUtil;
 import hu.bme.edu.handmade.services.impl.MyUserDetailsService;
 import hu.bme.edu.handmade.services.impl.UserService;
 import hu.bme.edu.handmade.web.dto.UserDto;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.IanaLinkRelations;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,25 +23,23 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin(origins = "http://localhost:4200")
 public class HomeController {
 
-    @Autowired
-    private AuthenticationManager authenticationManager;
+    private final AuthenticationManager authenticationManager;
+    private final JwtTokenUtil jwtTokenUtil;
+    private final MyUserDetailsService userDetailsService;
+    private final UserService userService;
+    private final UserModelAssembler assembler;
 
-    @Autowired
-    private JwtTokenUtil jwtTokenUtil;
-
-    @Autowired
-    private MyUserDetailsService userDetailsService;
-
-    @Autowired
-    UserService userService;
-
-    @RequestMapping("/hello")
-    public String firstPage() {
-        return "hello world,  JWT";
+    HomeController(AuthenticationManager authenticationManager, JwtTokenUtil jwtTokenUtil,
+                   MyUserDetailsService userDetailsService, UserService userService, UserModelAssembler assembler) {
+        this.authenticationManager = authenticationManager;
+        this.jwtTokenUtil = jwtTokenUtil;
+        this.userService = userService;
+        this.userDetailsService = userDetailsService;
+        this.assembler = assembler;
     }
 
     @PostMapping(value = "/authenticate")
-    public ResponseEntity<?> createAuthenticationToken(@RequestBody JwtRequest authenticationRequest) throws Exception {
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody JwtRequest authenticationRequest) throws AuthenticationException{
 
         authenticate(authenticationRequest.getUsername(), authenticationRequest.getPassword());
 
@@ -51,29 +53,21 @@ public class HomeController {
 
     @PostMapping(value = "/register")
     public ResponseEntity<?> saveUser(@RequestBody UserDto user) {
-        return ResponseEntity.ok(userService.registerNewUserAccount(user));
+        EntityModel<User> entityModel = assembler.toModel(userService.registerNewUserAccount(user));
+        return ResponseEntity
+                .created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri())
+                .body(entityModel);
     }
 
-    private void authenticate(String username, String password) throws Exception {
+    private void authenticate(String username, String password) throws AuthenticationException {
         try {
             authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
         } catch (DisabledException e) {
-            throw new Exception("USER_DISABLED", e);
+            throw new DisabledException("USER_DISABLED", e);
         } catch (BadCredentialsException e) {
-            throw new Exception("INVALID_CREDENTIALS", e);
+            throw new BadCredentialsException("INVALID_CREDENTIALS", e);
         }
     }
-
-    @RequestMapping("/")
-    public String greeting() {
-        return "Greetings from Spring Boot!";
-    }
-
-    @GetMapping("/home")
-    public String homeEndpoint() {
-        return "Home!";
-    }
-
 
 }
 
