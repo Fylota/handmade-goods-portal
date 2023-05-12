7ba1049f49e17a8b402656c9e5dcc21cf937ef33
Ágota Fülöp <fulop.agota01@gmail.com>
2023-05-12 20:21:08 +0200

    Added paging to posts

--- src/main/java/hu/bme/edu/handmade/controllers/PostController.java
+++ src/main/java/hu/bme/edu/handmade/controllers/PostController.java
@@ -6,11 +6,18 @@ import hu.bme.edu.handmade.web.dto.PostDto;
 import hu.bme.edu.handmade.web.dto.error.ResourceNotFoundException;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @CrossOrigin(origins="http://localhost:4200", maxAge=3600)
@@ -25,6 +32,40 @@ public class PostController {
     @GetMapping()
     public List<Post> getPosts() {
         return postService.findAllPosts();
+    }
+
+    @GetMapping("/page")
+    public ResponseEntity<Map<String, Object>> getPostPages(@RequestParam(defaultValue = "0") int page,
+                                                           @RequestParam(defaultValue = "3") int size,
+                                                           @RequestParam(defaultValue = "id,desc") String[] sort) {
+        try {
+            List<Sort.Order> orders = new ArrayList<>();
+            if (sort[0].contains(",")) {
+                for (String sortOrder : sort) {
+                    String[] _sort = sortOrder.split(",");
+                    orders.add(new Sort.Order(getSortDirection(_sort[1]), _sort[0]));
+                }
+            } else {
+                orders.add(new Sort.Order(getSortDirection(sort[1]), sort[0]));
+            }
+            Pageable paging = PageRequest.of(page, size, Sort.by(orders));
+            Page<Post> pagePosts = postService.findPages(paging);
+            List<Post> posts = pagePosts.getContent();
+
+            if (posts.isEmpty()) {
+                return ResponseEntity.noContent().build();
+            }
+
+            Map<String, Object> response = new HashMap<>();
+            response.put("data", posts);
+            response.put("currentPage", pagePosts.getNumber());
+            response.put("totalItems", pagePosts.getTotalElements());
+            response.put("totalPages", pagePosts.getTotalPages());
+
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
     }
 
     @GetMapping("/{id}")
@@ -60,5 +101,15 @@ public class PostController {
         catch (EmptyResultDataAccessException e) {
             return ResponseEntity.notFound().build();
         }
+    }
+
+    private Sort.Direction getSortDirection(String direction) {
+        if (direction.equals("asc")) {
+            return Sort.Direction.ASC;
+        } else if (direction.equals("desc")) {
+            return Sort.Direction.DESC;
+        }
+
+        return Sort.Direction.ASC;
     }
 }
--- src/main/java/hu/bme/edu/handmade/repositories/PostRepository.java
+++ src/main/java/hu/bme/edu/handmade/repositories/PostRepository.java
@@ -1,9 +1,9 @@
 package hu.bme.edu.handmade.repositories;
 
 import hu.bme.edu.handmade.models.Post;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PostRepository extends CrudRepository<Post, Long> {
+public interface PostRepository extends JpaRepository<Post, Long> {
 }
