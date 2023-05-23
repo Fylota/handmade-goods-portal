36c1bbefc0d3ea07814b10b3efe87343616ea695
Ágota Fülöp <fulop.agota01@gmail.com>
2023-05-23 14:28:53 +0200

    Deleted not user entity

--- src/main/java/hu/bme/edu/handmade/repositories/MessageRepository.java
+++
@@ -1,9 +0,0 @@
-package hu.bme.edu.handmade.repositories;
-
-import hu.bme.edu.handmade.models.Message;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MessageRepository extends CrudRepository<Message, Long> {
-}
