c7c3cecf16f58aa049055c09b8108f45d8b895cd
Ágota Fülöp <fulop.agota01@gmail.com>
2023-05-25 14:14:55 +0200

    updated profile/orders table

--- src/main/frontend/src/app/components/shop/orders-list/orders-list.component.ts
+++ src/main/frontend/src/app/components/shop/orders-list/orders-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input, OnChanges } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
+import { AfterViewInit, Component, Input, OnChanges, ViewChild } from '@angular/core';
+import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { Router } from '@angular/router';
 import { OrderControllerService, OrderItemDto, UserDto } from 'src/app/core/api/v1';
@@ -6,15 +8,26 @@ import { OrderControllerService, OrderItemDto, UserDto } from 'src/app/core/api/
 @Component({
   selector: 'app-orders-list',
   templateUrl: './orders-list.component.html',
-  styleUrls: ['./orders-list.component.scss']
+  styleUrls: ['./orders-list.component.scss'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
-export class OrdersListComponent implements OnChanges  {
+export class OrdersListComponent implements OnChanges, AfterViewInit  {
   @Input()
   user!: UserDto;
   myOrders?: OrderItemDto[];
 
   displayedColumns: string[] = ['id', 'creationDate', 'status', 'action'];
-  dataSource?: MatTableDataSource<OrderItemDto>;
+  columnsToDisplayWithExpand = [...this.displayedColumns, 'expand'];
+  expandedElement: OrderItemDto | undefined;
+  dataSource: MatTableDataSource<OrderItemDto> = new MatTableDataSource();
+  @ViewChild(MatSort)
+  sort: MatSort = new MatSort;
 
   constructor(
     private orderService: OrderControllerService,
@@ -28,8 +41,13 @@ export class OrdersListComponent implements OnChanges  {
         console.log(orders);
         this.myOrders = orders;
         this.dataSource = new MatTableDataSource(this.myOrders);
+        this.dataSource.sort = this.sort;
       });
     }
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
   }
 
   navigateToDetails(orderId: number): void {
